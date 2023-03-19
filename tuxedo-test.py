d81e4bcc647c60468f1dd806d33807b7153baad8
Nick Nelson <nicholas.g.nelson@exxonmobil.com>
2023-03-19 17:33:05 -0500

    Working on Mac

--- tuxedo-test.py
+++ tuxedo-test.py
@@ -12,6 +12,7 @@ from time import sleep
 import urllib
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL:@SECLEVEL=1'
 
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
@@ -21,8 +22,16 @@ import requests
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_NAME = "Tuxedo"
+DOMAIN = "https://192.168.4.42"
+API_REV = "API_REV01"
+API_BASE_PATH = DOMAIN + "/system_http_api/" + API_REV
 
+MAC = ""
+PRIVATE_KEY = ""
+API_KEY_ENC = PRIVATE_KEY[0:64]
+API_IV_ENC = PRIVATE_KEY[64:]
 
+CODE = ""
 
 def _sign_string(value, secret_key):
     return hmac.new(bytes(secret_key, "utf-8"), bytes(value, "utf-8"), sha1).hexdigest()
@@ -51,55 +60,67 @@ def api_call(api_route, params):
     header = "MACID:" + MAC + ",Path:" + API_REV + api_route
     authtoken = _sign_string(header, API_KEY_ENC)
 
-    return requests.post(
+    data = {
+        "param": uri_params_encrypted,
+        "len": len(urllib.parse.quote_plus(uri_params_encrypted)),
+        "tstamp": random.random()
+    }
+
+    headers = {
+        "authtoken": authtoken,
+        "identity": API_IV_ENC,
+        str(PRAGMA): "no-cache",
+        str(CACHE_CONTROL): "no-cache"
+    }
+
+    r = requests.post(
         full_url,
-        data={
-            "param": uri_params_encrypted,
-            "len": len(urllib.parse.quote_plus(uri_params_encrypted)),
-            "tstamp": random.random(),
-        },
-        headers={
-            "authtoken": authtoken,
-            "identity": API_IV_ENC,
-            PRAGMA: "no-cache",
-            CACHE_CONTROL: "no-cache",
-        },
+        data=data,
+        headers=headers,
         verify=False,
+        allow_redirects=False
     )
+    return r
 
-register_route = "/Registration/Register"
-register_params = {"mac": MAC, "operation": "set"}
-register_response = api_call(register_route, register_params)
-print(register_response)
-
-get_status_route = "/GetSecurityStatus"
-get_status_params = {"operation": "get"}
-get_status_response = api_call(get_status_route, get_status_params)
-print(_decrypt_data(json.loads(get_status_response.content)["Result"], API_KEY_ENC, API_IV_ENC))
-
-arm_with_code_route = "/AdvancedSecurity/ArmWithCode"
-arm_with_code_params = { "arming": "STAY", "pID": "1", "ucode": CODE, "operation": "set" }
-arm_with_code_response = api_call(arm_with_code_route, arm_with_code_params)
-print(arm_with_code_response)
-
-sleep(3)
+try:
+    register_route = "/Registration/Register"
+    register_params = {"mac": MAC, "operation": "set"}
+    register_response = api_call(register_route, register_params)
+    register_response.raise_for_status()
+    print(register_response)
+except Exception as e:
+    print("Registration failed")
+    print(e)
+    exit(0)
 
 get_status_route = "/GetSecurityStatus"
 get_status_params = {"operation": "get"}
 get_status_response = api_call(get_status_route, get_status_params)
 print(_decrypt_data(json.loads(get_status_response.content)["Result"], API_KEY_ENC, API_IV_ENC))
 
-disarm_with_code_route = "/AdvancedSecurity/DisarmWithCode"
-disarm_with_code_params = {"pID": "1", "ucode": CODE, "operation": "set"}
-disarm_with_code_response = api_call(disarm_with_code_route, disarm_with_code_params)
-print(disarm_with_code_response)
+# arm_with_code_route = "/AdvancedSecurity/ArmWithCode"
+# arm_with_code_params = { "arming": "STAY", "pID": "1", "ucode": CODE, "operation": "set" }
+# arm_with_code_response = api_call(arm_with_code_route, arm_with_code_params)
+# print(arm_with_code_response)
 
-sleep(3)
+# sleep(3)
 
-get_status_route = "/GetSecurityStatus"
-get_status_params = {"operation": "get"}
-get_status_response = api_call(get_status_route, get_status_params)
-print(_decrypt_data(json.loads(get_status_response.content)["Result"], API_KEY_ENC, API_IV_ENC))
+# get_status_route = "/GetSecurityStatus"
+# get_status_params = {"operation": "get"}
+# get_status_response = api_call(get_status_route, get_status_params)
+# print(_decrypt_data(json.loads(get_status_response.content)["Result"], API_KEY_ENC, API_IV_ENC))
+
+# disarm_with_code_route = "/AdvancedSecurity/DisarmWithCode"
+# disarm_with_code_params = {"pID": "1", "ucode": CODE, "operation": "set"}
+# disarm_with_code_response = api_call(disarm_with_code_route, disarm_with_code_params)
+# print(disarm_with_code_response)
+
+# sleep(3)
+
+# get_status_route = "/GetSecurityStatus"
+# get_status_params = {"operation": "get"}
+# get_status_response = api_call(get_status_route, get_status_params)
+# print(_decrypt_data(json.loads(get_status_response.content)["Result"], API_KEY_ENC, API_IV_ENC))
 
 unregister_route = "/Registration/Unregister"
 unregister_params = {"mac": MAC, "operation": "set"}
